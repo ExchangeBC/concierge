a510873d9492596edfc4a2369922957b512cef91
Dhruv Dang <hi@dhruv.io>
2019-02-13 22:27:14 -0800

    switch to simpler, standard dotenv usage

--- src/back-end/config.ts
+++ src/back-end/config.ts
@@ -1,15 +1,11 @@
 import { config } from 'dotenv';
-import { resolve } from 'path';
+
+config();
 
 const get = (name, fallback = undefined) => process.env[name] || fallback;
-const env = get('NODE_ENV', 'development');
-
-config({
-  path: resolve(__dirname, `../../${env}.env`)
-});
 
 export default {
-  env,
+  env: get('NODE_ENV', 'production'),
   port: parseInt(get('PORT', 3000), 10),
   mongoUrl: get('MONGO_URL', 'mongodb://dev:dev@localhost:27017/concierge'),
   secret: get('SECRET', 'foobar')
