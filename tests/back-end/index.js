f23ae25e5a3ba91e7556f8043b35282d4730906e
Dhruv Dang <hi@dhruv.io>
2019-02-20 15:30:11 -0800

    CON-80 CON-81 set up test harnesses for front-end and back-end

--- tests/back-end/index.js
+++ tests/back-end/index.js
@@ -1,6 +1,26 @@
-"use strict";
+const path = require('path');
 
-global.assert = require("assert");
-global._ = require("lodash");
+require('ts-node').register({
+  project: path.join(__dirname, './tsconfig.json')
+});
 
-require("./example");
+const Mocha = require('mocha');
+
+// Configure the mocha test-runner.
+// mocha.setup('bdd');
+const mocha = new Mocha({
+  ui: 'bdd',
+  globals: ['assert']
+});
+
+// Load all tests.
+const tests = [
+  'unit/example'
+];
+
+tests.forEach(t => mocha.addFile(path.join(__dirname, t)));
+
+// Run all tests.
+mocha.run(failures => {
+  process.exit(failures ? 1 : 0);
+});
--- tests/front-end/example.js
+++
@@ -1,9 +0,0 @@
-describe("Example", () => {
-  describe("action", () => {
-    context("when condition", () => {
-      it("outcome", () => {
-        assert(true, "The example is working!");
-      });
-    });
-  });
-});
