1a8d9097d1937387c5624410343da0fc508d123e
Dhruv Dang <hi@dhruv.io>
2019-03-04 23:23:19 -0800

    turn off debug messages in the front-end

--- src/front-end/typescript/index.ts
+++ src/front-end/typescript/index.ts
@@ -3,7 +3,7 @@ import { start } from 'front-end/lib/framework';
 import { throttle } from 'lodash';
 
 // Set the 3rd, boolean argument to true for debug mode.
-start(app, document.getElementById('main') || document.body, true)
+start(app, document.getElementById('main') || document.body, false)
   .then(stateManager => {
     // Throttle DOM queries.
     const querySelector = throttle((selector: string) => document.querySelector(selector), 500);
