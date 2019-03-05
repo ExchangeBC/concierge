269118c7427d8fb49e10d92b250991898045e643
Dhruv Dang <hi@dhruv.io>
2019-03-05 01:29:19 -0800

    stylefix

--- src/front-end/typescript/lib/pages/sign-up/components/account-information.tsx
+++ src/front-end/typescript/lib/pages/sign-up/components/account-information.tsx
@@ -106,7 +106,7 @@ const UserTypeRadio: View<{ state: State, userType: UserType }> = ({ state, user
   const isChecked = state.userType === userType;
   const style = { cursor: 'pointer' };
   return (
-    <Link href={`/sign-up/${userType.toLowerCase().replace('_', '-')}`} className='custom-radio custom-control pl-2' buttonClassName='p-0 d-flex align-items-center' textColor='body'>
+    <Link href={`/sign-up/${userType.toLowerCase().replace('_', '-')}`} className='custom-radio custom-control pr-2' buttonClassName='p-0 d-flex align-items-center' textColor='body'>
       <input
         id={id}
         type='radio'
