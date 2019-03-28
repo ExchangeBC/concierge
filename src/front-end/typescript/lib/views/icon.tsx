2529a4e059882211b2aef644d93463fcda61b320
Dhruv Dang <hi@dhruv.io>
2019-03-28 16:10:54 -0700

    CON-253 update calendar icon for rfi list view

--- src/front-end/typescript/lib/views/icon.tsx
+++ src/front-end/typescript/lib/views/icon.tsx
@@ -55,7 +55,7 @@ const Icon: View<Props> = props => {
     case 'chevron-left':
       return (<Feather {...props}><polyline points='15 18 9 12 15 6'></polyline></Feather>);
     case 'calendar':
-      return (<FontAwesome viewBox='0 0 448 512' {...props}><path fill='currentColor' d='M400 64h-48V8c0-4.4-3.6-8-8-8h-16c-4.4 0-8 3.6-8 8v56H128V8c0-4.4-3.6-8-8-8h-16c-4.4 0-8 3.6-8 8v56H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm16 400c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V192h384v272zm0-304H32v-48c0-8.8 7.2-16 16-16h352c8.8 0 16 7.2 16 16v48zM112 384h96c8.8 0 16-7.2 16-16v-96c0-8.8-7.2-16-16-16h-96c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16zm16-96h64v64h-64v-64z'></path></FontAwesome>);
+      return (<FontAwesome viewBox='0 0 448 512' {...props}><path fill='currentColor' d='M112 368h96c8.8 0 16-7.2 16-16v-96c0-8.8-7.2-16-16-16h-96c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V160h352v298z'></path></FontAwesome>);
     case 'check':
       return (<Feather {...props}><polyline points='20 6 9 17 4 12'></polyline></Feather>);
     case 'help-circle':
