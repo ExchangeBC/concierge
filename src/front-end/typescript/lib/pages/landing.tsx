11e8871d28f76f5ae90ee0ecee8e77c82daaef64
Dhruv Dang <hi@dhruv.io>
2019-03-29 15:21:07 -0700

    update landing page copy

--- src/front-end/typescript/lib/pages/landing.tsx
+++ src/front-end/typescript/lib/pages/landing.tsx
@@ -138,18 +138,8 @@ const Features: ComponentView<State, Msg> = ({ state, dispatch }) => {
       <Container>
         <Row className='justify-content-center'>
           <Col xs='12' md='4' className='pb-5 pb-md-0 px-4 d-flex flex-column justify-content-start align-items-center'>
-            <Icon name='matchmaking' color='secondary' width={4.5} height={4} />
-            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Coming Soon</small>
-            <h4 className='text-center mb-3 font-weight-bold w-100'>
-              Matchmaking
-            </h4>
-            <p className='text-center'>
-              The Program's staff will connect Public Sector Buyers and Vendors based on their profiles and areas of interest to facilitate pre-market engagement.
-            </p>
-          </Col>
-          <Col xs='12' md='4' className='pb-5 pb-md-0 px-4 d-flex flex-column justify-content-start align-items-center'>
             <Icon name='rfi' color='secondary' width={4} height={4} />
-            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Coming Soon</small>
+            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Available Now</small>
             <h4 className='text-center mb-3 font-weight-bold w-100'>
               Requests for Information
             </h4>
@@ -159,12 +149,22 @@ const Features: ComponentView<State, Msg> = ({ state, dispatch }) => {
           </Col>
           <Col xs='12' md='4' className='px-4 d-flex flex-column justify-content-start align-items-center'>
             <Icon name='discovery-day' color='secondary' width={3} height={4} />
-            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Coming Soon</small>
+            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Available Now</small>
             <h4 className='text-center mb-3 font-weight-bold w-100'>
               Discovery Days
             </h4>
             <p className='text-center'>
               The Program's staff will organise Discovery Day Sessions related to posted Requests for Information ("RFIs") to allow Vendors and Public Sector Buyers to meet and discuss them.
+            </p>
+          </Col>
+          <Col xs='12' md='4' className='pb-5 pb-md-0 px-4 d-flex flex-column justify-content-start align-items-center'>
+            <Icon name='matchmaking' color='secondary' width={4.5} height={4} />
+            <small className='text-uppercase text-center font-weight-bold text-secondary mt-3 mb-2 w-100'>Coming Soon</small>
+            <h4 className='text-center mb-3 font-weight-bold w-100'>
+              Matchmaking
+            </h4>
+            <p className='text-center'>
+              The Program's staff will connect Public Sector Buyers and Vendors based on their profiles and areas of interest to facilitate pre-market engagement.
             </p>
           </Col>
         </Row>
