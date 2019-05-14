ef28a9d74d871d0f07c2eadc5e9e4bb86e0b4abd
Dhruv Dang <hi@dhruv.io>
2019-05-13 17:03:50 -0700

    use envify for front-end debug mode

--- grunt-configs/browserify.js
+++ grunt-configs/browserify.js
@@ -3,36 +3,23 @@ const path = require("path");
 const root = path.resolve(__dirname, '..', gruntConfig.src.ts);
 const compilerOptions = require(path.join(root, 'tsconfig.json')).compilerOptions;
 const pathmodify = require("pathmodify");
+const envify = require("envify/custom");
 
 const makeConfig = debug => ({
   options: {
-    transform: [
-      [
-        "envify",
-        {
-          NODE_ENV: debug ? "development" : "production"
-        }
-      ]
-    ],
-    plugin: [
-      [
-        "tsify",
-        {
-          project: root
-        }
-      ],
-      // Modify paths because tsify does not fully support rewriting paths per
-      // tsconfig.json's `paths` option.
-      [
-        "pathmodify",
-        {
+    configure(b) {
+      return b
+        .plugin('pathmodify', {
           mods: [
             pathmodify.mod.dir("front-end", root),
             pathmodify.mod.dir("shared", path.resolve(root, "../../shared"))
           ]
-        }
-      ]
-    ],
+        })
+        .plugin('tsify', { project: root })
+        .transform(envify({
+          NODE_ENV: debug ? 'development' : 'production'
+        }), { global: true });
+    },
     browserifyOptions: {
       debug,
       extensions: [".js", ".json", ".ts", ".tsx", ".jsx"]
--- src/front-end/typescript/index.ts
+++ src/front-end/typescript/index.ts
@@ -4,7 +4,7 @@ import { set } from 'lodash';
 
 const element = document.getElementById('main') || document.body;
 // TODO use environment variables.
-const debug = false;
+const debug = process.env.NODE_ENV === 'development';
 start(app, element, debug)
   .then(stateManager => {
     // Store stateManager on the window in development.
