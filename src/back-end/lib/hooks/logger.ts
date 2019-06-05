2e2abfb0f76ab93ce3f026c11b65809a128fbe45
Dhruv Dang <hi@dhruv.io>
2019-06-04 22:43:32 -0700

    only log session id for requests

--- src/back-end/lib/hooks/logger.ts
+++ src/back-end/lib/hooks/logger.ts
@@ -1,10 +1,11 @@
+import { Session } from 'back-end/lib/app/types';
 import { RouteHook } from 'back-end/lib/server';
 import chalk from 'chalk';
 
-const hook: RouteHook<unknown, unknown, number, object>  = {
+const hook: RouteHook<unknown, unknown, number, Session>  = {
 
   async before(request) {
-    request.logger.info(`${chalk.gray('->')} ${request.method} ${request.path}`, request.session);
+    request.logger.info(`${chalk.gray('->')} ${request.method} ${request.path}`, { sessionId: request.session.sessionId });
     return Date.now();
   },
 
