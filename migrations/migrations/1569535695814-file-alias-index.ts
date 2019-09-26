5a73ff24fd2caa5c56562e259d9b6e760c2faf71
Dhruv Dang <hi@dhruv.io>
2019-09-26 15:14:50 -0700

    add index on file aliases

---
+++ migrations/migrations/1569535695814-file-alias-index.ts
@@ -0,0 +1,25 @@
+import { makeDomainLogger } from 'back-end/lib/logger';
+import { console as consoleAdapter } from 'back-end/lib/logger/adapters';
+import { MigrationHook } from 'migrate';
+import { connect } from 'migrations/db';
+import path from 'path';
+
+const logger = makeDomainLogger(consoleAdapter, `migrations:${path.basename(__filename)}`);
+
+export const description = 'Creates an index on the file collection\'s "alias" property.';
+
+const indexName = 'fileAlias';
+
+export const up: MigrationHook = async () => {
+  const { db } = await connect();
+  logger.info('creating index...');
+  await db.collection('files').createIndex({ alias: 1 }, { name: indexName });
+  logger.info('...index created.');
+};
+
+export const down: MigrationHook = async () => {
+  const { db } = await connect();
+  logger.info('dropping index...');
+  await db.collection('files').dropIndex(indexName);
+  logger.info('...index dropped.');
+};
